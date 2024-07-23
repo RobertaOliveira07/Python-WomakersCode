b192ebe40f7c944c630e1103501e8fcbf2164aff
Roberta <roberttavalentim07@icloud.com>
2024-07-23 19:21:25 -0300

    Estudos de operações matemáticas e resto, incremento e decremento

---
+++ inicio.py
@@ -0,0 +1,53 @@
+'''Mensagem inicial para o usuario
+print( 'Olá mundo!')
+print( 'Olá, esse é meu primeiro código em python')'''
+
+'''Tipo inteiro - int
+numero = int(input('Digite seu numero:'))
+print(numero)'''
+
+# Tipo float - com vírgula
+
+'''numero = float(input('Digite um numero float:'))
+print(numero) '''
+
+#Tipo string - textual
+
+'''frase = input('Digite sua frase:')
+print(frase)'''
+
+#Operações matemáticas
+
+# Soma =  +
+
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+soma = numero1+numero2
+print(soma)'''
+
+# Subtração = -
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+subtração = numero1-numero2
+print(subtração)'''
+
+#Multiplicação = *
+
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+multiplicar = numero1*numero2
+print(multiplicar)'''
+
+#Divisão = /
+
+'''numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+dividir = numero1/numero2
+print(dividir)'''
+
+#Divisão inteira = //
+
+numero1 = int(input('Digite o numero 01:'))
+numero2 = int(input('Digite o numero 02:'))
+dividir = numero1//numero2
+print(dividir)
---
+++ operadores.py
@@ -0,0 +1,9 @@
+#resto
+#incremento
+'''valor = 5
+valor +=1
+print(valor)'''
+#decremento
+valor = 5
+valor -=1
+print(valor)
