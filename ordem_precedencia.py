287a8facd40a0b8396024398c83734acfa6cabab
Roberta <roberttavalentim07@icloud.com>
2024-07-23 19:41:21 -0300

    estudo de ordem de precedencia e operadores relacionais

---
+++ ordem_precedencia.py
@@ -0,0 +1,13 @@
+# ordem de precedencia
+
+calculo = (2+5)*((5+4)*3)
+print(calculo)
+
+#operadores relacionais
+
+# == : igual
+# > : maior
+# >= : maior igual
+# < : menor
+# <= : menor igual
+# != : diferente
